50fcaa4f25e2d1770e1fe7e5d0bc2cad0b4051b3
tingliang <ting.liang@ximalaya.com>
2018-03-19 19:21:22 +0800

    concat

---
+++ src/main/java/CodeFragment.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.OptionalInt;
+import java.util.stream.Stream;
+
+public class CodeFragment {
+
+    /****** gcd *****/
+    public static OptionalInt gcd(int[] numbers){
+        return Arrays.stream(numbers)
+                .reduce((a,b)->gcd(a,b));
+
+    }
+
+    private static int gcd(int a, int b){
+
+        if (b == 0){
+            return a;
+        }
+        return gcd(b, a%b);
+    }
+
+    /*** concat ****/
+    public static <T> T[] concat(T[] first, T[] second){
+
+        return Stream.concat(
+                Stream.of(first),
+                Stream.of(second)
+        ).toArray( i->{
+            System.out.println(i);
+            return (T[])Arrays.copyOf(new Object[0], i, first.getClass());
+        });
+    }
+
+}
